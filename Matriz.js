fd1a990a245c6ccb8ca4859c01984950925f15bb
Gabriel Aquino <99772558+gabriel08snts@users.noreply.github.com>
2022-09-30 11:47:57 -0300

    Add files via upload

---
+++ Matriz.js
@@ -0,0 +1,9 @@
+//UMA MATRIX É UM ARRAY BIDIMENSIONAL;
+const students = [
+  ['Joseph', 25, 'MG'],
+  ['Briana', 28, 'SP'],
+  ['Priscila', 26, 'PR']
+]
+
+//ESCOLHENDO UM DOS ARRAYS E DEPOIS UM ELEMENTO;
+console.log([0], [1])
