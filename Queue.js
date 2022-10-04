8eddafae9455f247dc7df72a1f60d85b8d89c1cf
Gabriel Aquino <99772558+gabriel08snts@users.noreply.github.com>
2022-10-04 18:23:54 -0300

    Add files via upload

---
+++ Queue.js
@@ -0,0 +1,26 @@
+//1º PASSO: MODELAGEM;
+class Queue {
+  constructor() {
+    this.data = []
+  }
+
+  enqueue(item) {
+    this.data.push(item)
+    console.log(`${item} chegou na fila!`)
+  }
+
+  dequeue() {
+    const item = this.data.shift()
+    console.log(`${item} saiu da fila!`)
+  }
+}
+
+//2º PASSO: UTILIZANDO;
+const fila = new Queue()
+
+fila.enqueue('Mariana')
+fila.enqueue('Joao')
+fila.enqueue('Ariel')
+fila.dequeue('')
+fila.dequeue('')
+fila.dequeue('')
